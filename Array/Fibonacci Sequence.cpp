7a81c27c91a5ba68e8c2ab124d5d9d771d76a38c
KA-EuNs <rkdms8244@naver.com>
2021-01-25 21:22:57 +0900

    Array application

---
+++ Array/Fibonacci Sequence.cpp
@@ -0,0 +1,25 @@
+//#include<stdio.h>
+//void main() {
+//	void fibo(int);
+//	int N;
+//	printf("type an integer:");
+//	scanf_s("%d", &N);
+//	fibo(N);
+//}
+//
+//void fibo(int num)
+//{
+//	int first = 0, second = 1, next, k;
+//
+//	for (k = 0; k < num; k++) {
+//		if (k <= 1)
+//			next = k;
+//		else
+//		{
+//			next = first + second;
+//			first = second;
+//			second = next;
+//		}
+//		printf("%d\n", next);
+//	}
+//}
