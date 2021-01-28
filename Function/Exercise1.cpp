d3e460e9f1b92df7d3c335718b85d5fd695a9cf8
KA-EuNs <rkdms8244@naver.com>
2021-01-28 21:49:08 +0900

    Function Exercise

---
+++ Function/Exercise1.cpp
@@ -0,0 +1,29 @@
+//#include<stdio.h>
+//void add2Array(int, int[], int[], int[]);
+//void printArray(int[], int);
+//void main() {
+//	int arr1[3] = { 1,2,3 };
+//	int arr2[3] = { 3,2,1 };
+//	int arr3[3];
+//
+//	printArray(arr1, 3);
+//	printArray(arr2, 3);
+//	add2Array(3, arr1, arr2, arr3);
+//	printArray(arr3, 3);
+//}
+//
+//void printArray(int a1[], int size)
+//{
+//	for (int i = 0; i < size; i++) {
+//		printf("%d ", a1[i]);
+//	}
+//	printf("\n");
+//}
+//
+//void add2Array(int size, int a1[], int a2[], int a3[])
+//{
+//	for (int j = 0; j < size; j++) {
+//		a3[j] = a1[j] + a2[j];
+//	}
+//}
+//
